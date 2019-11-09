d62e115dd8f3a0d19c86f5329559dec3e33b7c0f
Madison Davis <madison.davis@my.maryvillecollege.edu>
2019-11-08 21:55:49 -0500

    Initial commit

---
+++ src/main/java/api/ForumsEndpoint.java
@@ -0,0 +1,14 @@
+package api;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/Forums")
+public class ForumsEndpoint {
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+	return "Hello from Jersey";
+	}
+}
