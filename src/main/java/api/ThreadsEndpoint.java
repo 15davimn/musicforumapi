aed035052dbab1c6a2bd164e5d49ce6193bb8e25
15davimn <48268658+15davimn@users.noreply.github.com>
2019-11-13 19:28:24 -0500

    Update ThreadsEndpoint.java

--- src/main/java/api/ThreadsEndpoint.java
+++ src/main/java/api/ThreadsEndpoint.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
